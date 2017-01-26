20082cc8ea07840488ec039ea1fa5c9ff583d80d
ahnuce <ahnuce@gmail.com>
2017-01-26 10:48:07 -0500

    made partials and index partial shows

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 (function(){
 angular
- .module('sixteenBars', ["ui.router", ])
- .config(["$stateProvider", Router])
+  .module('sixteenBars', ["ui.router", ])
+  .config(["$stateProvider", Router])
 
- function Router($stateProvider){
-   console.log("working");
-   $stateProvider
-   .state('index', {
-       url: '/poems',
-       templateUrl: '/templates/poemsIndex.html',
-       controller: "PoemsCtrl"
-   })
-   .state('show', {
-       url: '/poems/:id',
-       templateUrl: '/templates/poemShow.html',
-       controller: "PoemsCtrl",
-   })
-   
- }
+  function Router($stateProvider){
+    console.log("working");
+    $stateProvider
+    // The state that shows all poems.
+    .state('index', {
+        url: '/poems',
+        templateUrl: '/partials/poemsIndex.html',
+    })
+    // The state that shows one poem. / delete is buttton within this state.
+    .state('show', {
+        url: '/poem',
+        templateUrl: '/partials/poemShow.html',
+    })
+    // Tbe state that shows new poem form.
+    .state('create', {
+        url: '/poemNew',
+        templateUrl: '/partials/poemNew.html',
+    })
+    // The state that shows edit.
+    .state('edit', {
+        url: '/poemEdit',
+        templateUrl: '/partials/poemEdit.html',
+    })
+  }
 })();
--- js/poems.controller.js
+++ js/poems.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function(){
     angular
         .module('sixteenBars')
         .controller('PoemsCtrl', ['$http', '$scope', PoemsCtrl]);
