83103a53b9f5c34c5352293ac8e8f853b42079b3
ahnuce <ahnuce@gmail.com>
2017-01-26 09:08:51 -0500

    added some states with ui router and stateprovider and also show db table

--- js/poems.controller.js
+++ js/poems.controller.js
@@ -1,22 +1,20 @@
 (function() {
     angular
         .module('sixteenBars')
-        .controller('PoemsCtrl', ['$http', '$scope', PoemsCtrl]);
-
-    PoemsCtrl.$inject = ['$http', '$scope'];
-
-    function PoemsCtrl($http, $scope) {
+        .controller('PoemCtrl', ['$http', '$scope', PoemCtrl]);
+    function PoemCtrl($http, $scope) {
 
     $scope.getPoems = function(){
       $http.get('http://localhost:3000/api/poems')
-      .then(function(allPoems){
-        $scope.Poems = allPoems.data;
-            })
-          }
-          $scope.showPoem = function(id){
+      .then(function(res){
+        $scope.poems = res.data;
+        console.log($scope.poems)
+      });
+          };
+          $scope.showPoems = function(id){
       $http.get(`http://localhost:3000/api/poems/${_id}`)
-        .then(function(allPoems){
-          $scope.Poem = allPoems.data.find({poem});
+        .then(function(res){
+          $scope.poem = res.data.find({poem});
         });
     };
 
@@ -29,27 +27,21 @@
 
         });
     };
-    // //create
-    // $scope.createGrumble = function(grumble){
-    // $http.post(`${rootURL}/grumbles`, grumble)
-    //   .then(function(res){
-    //     $scope.grumbles = res.data;
-    //     console.log($scope.grumbles);
-    //   })
-    //   .catch(function(err){
-    //     if(err)console.log(err);
-    //     });
-    // };
-    // //edit
-    // $scope.editGrumble = function(grumble){
-    // $http.put(`${rootURL}/grumbles/${$scope.grumble.id}`, grumble)
-    //   .then(function(res){
-    //     //$scope.getGrumbles(); //redirect to index
-    //     $scope.grumble = res.data;
-    //           console.log($scope.grumbles);
-    //   })
-    //   .catch(function(err){
-    //     if(err)console.log(err);
-    //     });
+    //create
+    $scope.createPoems = function(poem){
+    $http.post(`http://localhost:3000/api/poems/`, poem)
+      .then(function(res){
+        $scope.poems = res.data;
+        console.log($scope.poems);
+      });
     };
+    //edit
+    $scope.editPoems = function(poem){
+    $http.put(`http://localhost:3000/api/poems/${$scope.poem.id}`, poem)
+      .then(function(res){
+        $scope.poem = res.data;
+              console.log($scope.poems);
+      });
+    };
+  };
 })();
