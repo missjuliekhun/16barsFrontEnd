d7401eeaa16fc66dc649cf4b7a2b5a080bbcdacc
myztajay <myztajay@hotmail.com>
2017-01-26 17:29:35 -0500

    refactored with rooturl variable nice to have with heroku deployment

--- js/poems.controller.js
+++ js/poems.controller.js
@@ -3,32 +3,32 @@
         .module('sixteenBars')
         .controller('PoemsCtrl', ['$http', '$scope', '$state', PoemsCtrl]);
     function PoemsCtrl($http, $scope, $state) {
+      var rootURL = 'https://sixteen-bars.herokuapp.com/api/poems/';
 //index
     $scope.getPoems = function(){
-      $http.get('http://localhost:3000/api/poems')
+      $http.get(rootURL)
       .then(function(res){
         $scope.poems = res.data;
       });
           };
 //show
     $scope.showPoem = function(id){
-      $http.get(`http://localhost:3000/api/poems/${id}`)
+      $http.get(rootURL + id)
         .then(function(res){
           $scope.poem = res.data;
           $state.go('show')
         });
     };
 //delete
-    $scope.destroyPoem = function(id){
-      $http.delete(`http://localhost:3000/api/poems/${_id}`)
+    $scope.destroyPoem = function(poem){
+      $http.delete(rootURL +poem._id)
         .then(function(res){
-          $scope.poems = undefined;
-          $scope.getPoems();
+          $state.go('index')
         });
     };
 //create
     $scope.createPoem = function(poem){
-    $http.post(`http://localhost:3000/api/poems/`, poem)
+    $http.post(rootURL , poem)
       .then(function(res){
         $scope.poems = res.data;
         console.log($scope.poems);
@@ -36,7 +36,7 @@
     };
 //edit - only works the first time, not sure why?
     $scope.editPoem = function(poem){
-     $http.put(`http://localhost:3000/api/poems/${poem._id}`, poem)
+     $http.put(rootURL + poem._id, poem)
       .then(function(res){
         $scope.poem = res.data;
       });
