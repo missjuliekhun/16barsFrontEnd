cb18e3dc639a17eadaa526b8736709af963000ec
ahnuce <ahnuce@gmail.com>
2017-01-26 22:46:00 -0500

    created redirect for creating new poem

--- js/poems.controller.js
+++ js/poems.controller.js
@@ -4,6 +4,7 @@
         .controller('PoemsCtrl', ['$http', '$scope', '$state', PoemsCtrl]);
     function PoemsCtrl($http, $scope, $state) {
       var rootURL = 'https://sixteen-bars.herokuapp.com/api/poems/';
+
 //index
     $scope.getPoems = function(){
       $http.get(rootURL)
@@ -21,16 +22,21 @@
     };
 //delete
     $scope.destroyPoem = function(poem){
-      $http.delete(rootURL +poem._id)
+      $http.delete(rootURL + poem._id)
         .then(function(res){
+          $scope.poem = undefined;
+                $scope.getPoems();
           $state.go('index')
-        });
-    };
+});
+        };
+
 //create
     $scope.createPoem = function(poem){
     $http.post(rootURL , poem)
       .then(function(res){
         $scope.poems = res.data;
+          $state.go('index')
+
         console.log($scope.poems);
       });
     };
@@ -40,6 +46,7 @@
       .then(function(res){
         $scope.poem = res.data;
       });
-    };
   };
+
+}
 })();
